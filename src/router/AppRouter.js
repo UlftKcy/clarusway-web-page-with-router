7d603301d5600f6f6ca57bdcfcac6252274c2961
UlfetKacay <ukacay87@gmail.com>
2021-09-08 21:35:28 +0300

    router updated

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,4 +1,7 @@
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "../components/Navbar"
+import Subscribe from "../components/Subscribe";
+import Footer from "../components/Footer";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Services from "../pages/Services";
@@ -8,14 +11,19 @@ import Javascript from "../pages/Javascript";
 
 const AppRouter = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/services" component={Services} />
-      <Route path="/html" component={Html} />
-      <Route path="/css" component={Css} />
-      <Route path="/javascript" component={Javascript} />
-    </Switch>
+    <Router basename="clarusway">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
+        <Route path="/html" component={Html} />
+        <Route path="/css" component={Css} />
+        <Route path="/javascript" component={Javascript} />
+      </Switch>
+      <Subscribe />
+      <Footer />
+    </Router>
   );
 };
 export default AppRouter;
